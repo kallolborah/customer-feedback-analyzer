04207500966ee2bc68d90d454e7a33874a440c2f
Kallol Borah <kallol.borah@gmail.com>
2022-01-13 11:48:15 +0530

    added nodejs lambda function for posting customer feedback

--- sentiment-analysis/src/main/java/org/product/feedback/App.java
+++ sentiment-analysis/src/main/java/org/product/feedback/App.java
@@ -15,7 +15,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        String text = "It is raining today in Seattle";
+        String text = "I've always had a need for a good mouse. Classic mouse have given me wrist pain after especially long sessions. First and foremost, it looks and feels fantastic in my hand. It didn't take me long to get used to the new position. It felt completely normal after just a few minutes, and it is very light. The mouse's overall body is made of a plastic polymer, making it both durable and light. The textured surface strengthens grip and makes the overall user experience more pleasant. The left and right buttons are around the right side, with the scroll wheel in the center. The scroll wheel is covered with the same rubber as the rest of the mouse. It's silky smooth, with a subtle click. Two more adjustable buttons can be found around the left side. These are located just above your thumb and are easy to hit, even if your hands are small. To save battery life, it has a simple on/off button on the bottom. This mouse is fantastic in general. I've never used a mouse as comfortable as this one. Definitely recommend this mouse if you want a comfortable and easy-to-use product!";
 
         // Create credentials using a provider chain. For more information, see
         // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/credentials.html
